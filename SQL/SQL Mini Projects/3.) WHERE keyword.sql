92d89089686a36c13c51b9dc00106d2f4c5ee2e1
codeson101001 <43733930+codeson101001@users.noreply.github.com>
2020-09-01 13:10:21 -0500

    Two mini programs in one push

---
+++ SQL/SQL Mini Projects/3.) WHERE keyword.sql
@@ -0,0 +1,25 @@
+-- Get the orders placed this year
+
+USE sql_store;
+
+SELECT*
+FROM orders
+
+-- This will iterate through all of the order_date in the orders table
+-- >
+-- <=
+-- >=
+-- <
+-- != or <>
+
+
+-- Looking at the colum order_date where the date is after '2019-01-01'
+WHERE order_date>= '2019-01-01'
+
+
+
+-- FROM Customers
+-- WHERE state = 'VA'
+
+
+
